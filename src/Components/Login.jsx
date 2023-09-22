4c80d089087d105be3a2eb35e902f94b0bc51482
AnushkaDarshana <2018is087@stu.ucsc.cmb.ac.lk>
2023-09-22 15:53:06 +0530

    login

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,14 +1,30 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState  } from 'react';
 import { UserContext } from './UserProvider';
 
 export default function Login() {
 
-  const { login } = useContext(UserContext);
+  const { login, error } = useContext(UserContext);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(username, password);
+  };
   return (
     <div>
-      <button onClick={() => login({ name: 'John Doe', email: 'john.doe@example.com' })}>
-        Login
-      </button>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </label>
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+      {error && <p>{error}</p>}
     </div>
   )
 }
